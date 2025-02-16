129ba30cc2abc53ce00429e643095cd8f90e16b4
juj7347 <juj7347@naver.com>
2025-02-16 23:39:28 +0900

    ItemFactory

--- 2025-12-12/Java/src/main/java/com/gildedrose/Factory/ItemFactory.java
+++ 2025-12-12/Java/src/main/java/com/gildedrose/Factory/ItemFactory.java
@@ -1,3 +1,13 @@
+package com.gildedrose;
+
 public class ItemFactory {
-    private final static Map<String, BiFunction<String, String, ?>>
+    //각 item type별로 별도의 기능을 수행하는 함수 저장
+    private final static Map<String, BiFunction<int, int, Item>> itemMap = new HashMap<>();
+
+    itemMap.put("Sulfuras", (sellIn, quality) -> );
+    //파라메터는 createItem에 따라 그때그때
+    public Item createItem(String name, int sellIn, int quality) {
+
+    }
+
 }
